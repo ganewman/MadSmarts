7507cdf5745b0154f68adbb425b92d7fdb0789f4
Jeffrey Lin <jeffrey@icurse.nl>
2017-02-13 19:36:44 -0500

    Add skeleton

---
+++ MergeSortTester.java
@@ -0,0 +1,29 @@
+/** MergeSortTester.
+ *
+ * ALGORITHM:
+ *
+ *
+ * BIG-OH CLASSIFICATION OF ALGORITHM:
+ *
+ *
+ * Mean execution times for dataset of size n:
+ *  Batch size:
+ *  n=1         time:
+ *  n=10        time:
+ *  n=100       time:
+ *  n=1000000   time:
+ *
+ *  ANALYSIS:
+ *
+ */
+
+public class MergeSortTester {
+
+    /** Execution time analysis.
+     *
+     */
+    public static void main( String[] args ) {
+
+    }
+}
+
