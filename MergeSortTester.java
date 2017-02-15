8cb9fb6e62ae84ae269e3dab623a01ecdeec694c
Jeffrey Lin <jeffrey@icurse.nl>
2017-02-14 20:05:49 -0500

    Add testing mechanism

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -18,12 +18,34 @@
  */
 
 public class MergeSortTester {
+    private static final long ROUNDS = 10000;
+    private static final int  SIZE   = 1000 + (int)( Math.random() * 1000 );
 
     /** Execution time analysis.
      *
      */
     public static void main( String[] args ) {
+        System.out.println("Running " + ROUNDS + " rounds of testing.\n");
+        System.out.format("%s%30s%n", "Length", "Time (nanoseconds)");
 
+        long time = 0;
+        long s = 0;
+        long e = 0;
+        int[] data = new int[SIZE];
+
+        for ( long rounds = ROUNDS; rounds > 0; rounds-- ) {
+            for ( int i = 0; i < data.length; i++ ) {
+                data[i] = (int) ( 1000 * Math.random() );
+            }
+
+            s = System.nanoTime();
+            MergeSort.sort(data);
+            e = System.nanoTime();
+
+            time += (e - s);
+        }
+
+        System.out.format("%s%20s%n", data.length, time / ROUNDS);
     }
 }
 
