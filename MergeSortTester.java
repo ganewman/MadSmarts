ded54bcb4562a470e4e222992ab39f4f30e2718a
Jeffrey Lin <jeffrey@icurse.nl>
2017-02-15 00:02:09 -0500

    Add test results
    Remove unnecessary files. They are kept in the 'old' branch.

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -1,28 +1,43 @@
+/* Team MadSmarts -- Jeffrey Lin, Leo Liu, Gabi Newman, Allard Peng
+ * APCS2 period 03
+ * LAB00 -- What Does the Data Say?
+ * 2017-02-14
+ */
+
 /** MergeSortTester.
  *
  * ALGORITHM:
- *
+ * Take the array and split it into two arrays that are equivalent in size.
+ * Continue splitting each array until they have two items each, keeping track
+ * of the parent array.  Sort each individual two-item array.  Merge two decks
+ * together to form the parent pile.  Continue to merge until there is one
+ * sorted deck.
  *
  * BIG-OH CLASSIFICATION OF ALGORITHM:
- *
+ * O(n)
  *
  * Mean execution times for dataset of size n:
  *  Batch size:
- *  n=1         time:
- *  n=10        time:
- *  n=100       time:
- *  n=1000000   time:
+ *  n=    1     time:     361 nanoseconds
+ *  n=   10     time:    3253 nanoseconds
+ *  n=  100     time:   32765 nanoseconds
+ *  n=10000     time: 4002642 nanoseconds
  *
- *  ANALYSIS:
- *
+ * ANALYSIS:
+ * We can run various regressions to see which line of best fit is most
+ * applicable to the data. If we run a Linear Regression (ax+b) on the data
+ * above, we get the line $y = 400.5276654x - 2678.556315$ with an r value of
+ * 0.9999973273. This suggests that the data creates a linear trend.
  */
 
 public class MergeSortTester {
     private static final long ROUNDS = 10000;
-    private static final int  SIZE   = 1000 + (int)( Math.random() * 1000 );
+    private static final int    SIZE = 1000 + (int)( Math.random() * 1000 );
 
     /** Execution time analysis.
-     *
+     * Create an array of size SIZE and populate it with arbitary values and
+     * run merge sort on the array. Repeat ROUNDS times, then divide the total
+     * amount of time spent by ROUNDS to obtain the average.
      */
     public static void main( String[] args ) {
         System.out.println("Running " + ROUNDS + " rounds of testing.\n");
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # MadSmarts
+
+To find the execution time of the algorithm, we created a small program to run
+the algorithm many times, and found the average by dividing the total time
+spent by the number of times the algorithm was run. The table below contains
+times returned from one run of the program for each dataset size.
+
+| Dataset size | Time (nanoseconds) |
+| -----------: | -----------------: |
+|     1        |              361   |
+|    10        |             3253   |
+|   100        |            32765   |
+| 10000        |          4002642   |
+
+Running a linear regression on the data above gives us a linear equation with
+an r value of almost 1 (0.9999973273), so it is safe to assume that the Big-Oh
+classification of merge sort is **O(n)**.
+
